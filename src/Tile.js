3da8553a70db7e3531fbac0714d9912d65cd9285
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-02 19:44:49 -0700

    Adds the metadata

--- src/Tile.js
+++ src/Tile.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { getSprite } from 'pokemon-images';
+import PropTypes from 'prop-types';
+import metadata from 'pokemon-metadata';
 import { Card } from 'antd';
 
 function Tile({ name }) {
   return(
-    <Card title={name} style={{ width: 300, margin:'10px' }} bodyStyle={{display:'flex', justifyContent:'center'}}>
-      <img src={getSprite(name)} alt={name}/>
+    <Card
+      title={name}
+      extra={<span>{metadata[name].id}</span>}
+      style={{ width: 300, margin:'10px' }}
+      bodyStyle={{display:'flex', justifyContent:'center'}}
+    >
+      <img src={metadata[name].sprites.front_shiny} alt={name}/>
     </Card>
   );
 }
 
 Tile.propTypes = {
-  name: React.PropTypes.string,
+  name: PropTypes.string,
 }
 
 Tile.defaultProps = {
